46e9ac04eb90ae4bbc7a75cfce23c7c6e4c2db86
Jefferson Akira Fukamizu <58380777+aks2512@users.noreply.github.com>
2023-02-04 15:01:18 -0300

    Ajuste index.js

--- countdown/launch-countdown-timer-main-flipclock/index.js
+++ countdown/launch-countdown-timer-main-flipclock/index.js
@@ -1,5 +1,8 @@
-var date = new Date('10/2/2023'),
-clock = $('#flip-clock').FlipClock(date,{
+var today = new Date(); //Current Date
+var future = new Date("November 2, 2023"); //Set Timer Here
+var diff = future.getTime()/1000 - today.getTime()/1000;
+
+var clock = $('#flip-clock').FlipClock(diff,{
 	clockFace: 'DailyCounter',
 	countdown: true,
 	showSeconds: true,
