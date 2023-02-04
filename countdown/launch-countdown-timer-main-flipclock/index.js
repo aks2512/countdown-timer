8c6f924534bd698f70655ed26bce954fed2a712f
Jefferson Akira Fukamizu <58380777+aks2512@users.noreply.github.com>
2023-02-04 14:41:12 -0300

    Ajuste index.js

--- countdown/launch-countdown-timer-main-flipclock/index.js
+++ countdown/launch-countdown-timer-main-flipclock/index.js
@@ -3,8 +3,7 @@ days = date.getDate()*60*60*24,
 hours = date.getHours()*60*60,
 minutes = date.getMinutes()*60,
 sec = date.getSeconds(),
-
-var clock = $('#flip-clock').FlipClock({
+clock = $('#flip-clock').FlipClock({
 	clockFace: 'DailyCounter',
 	countdown: true,
 	showSeconds: true,
