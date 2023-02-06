9506c8f7e29c21118890de845158ae2b2d76e5cd
mzakira <jefferson.akira@mzgroup.com>
2023-02-06 09:17:51 -0300

    ajuste index.js

--- countdown/launch-countdown-timer-main-flipclock/index.js
+++ countdown/launch-countdown-timer-main-flipclock/index.js
@@ -1,9 +1,10 @@
 var today = new Date(); //Current Date
-var future = new Date("2022-02-7T23:50:21.817Z"); //Set Timer Here
+var future = new Date("2023-02-09T03:24:00"); //Set Timer Here
+console.log(future)
 var diff = future.getTime()/1000 - today.getTime()/1000;
 
 var clock = $('#flip-clock').FlipClock(diff,{
-	clockFace: 'HourlyCounter',
+	clockFace: 'DailyCounter',
 	countdown: true,
 	showSeconds: true
 });
