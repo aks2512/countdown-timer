3da25a927743cb3a80c33d0f210f8b5ba478ea7c
Jefferson Akira Fukamizu <58380777+aks2512@users.noreply.github.com>
2023-04-04 21:26:40 -0300

    Update index.js

--- countdown/launch-countdown-timer-main-flipclock/index.js
+++ countdown/launch-countdown-timer-main-flipclock/index.js
@@ -1,5 +1,5 @@
 var today = new Date(); //Current Date
-var future = new Date("2023-02-09T03:24:00"); //Set Timer Here
+var future = new Date("2023-05-09T03:24:00"); //Set Timer Here
 console.log(future)
 var diff = future.getTime()/1000 - today.getTime()/1000;
 
